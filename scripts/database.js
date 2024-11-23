0e715672047362fbbd4174cd6d6cb89a113d554f
A1f13n <alflen.fillipi08@gmail.com>
2024-11-22 21:32:54 -0300

    Adição de alguns CRUDS
    Adição de CRUDs para o pessoa, funcionario e iniciando o do hospede

--- scripts/database.js
+++ scripts/database.js
@@ -1,8 +1,66 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config() 
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL.PASSWORD,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-})
+}).promise()
+
+//CRUD PESSOA
+export async function insert_pessoa(nome, cpf, login, senha, data_nasc, telefone, email) {
+    // VALIDACAO AQUI
+    const [rows] = await pool.query('INSERT INTO pessoa(nome, cpf, login, senha, data_nasc, telefone, email VALUES(?, ?, ?, ?, ?, ?, ?);')
+    [nome, cpf, login, senha, data_nasc, telefone, email]
+    return rows
+}
+
+export async function select_pessoa(id) {
+    const [rows] = await pool.query("SELECT * FROM pessoa where id = ?;", [id])
+    return rows
+}
+
+export async function update_pessoa(coluna, informacao, id) {
+    // VALIDACAO AQUI
+    const [rows] = await pool.query("UPDATE pessoa SET ? = ? WHERE id = ?;", 
+        [coluna, informacao, id])
+    return rows
+}
+
+export async function delete_pessoa(id) {
+    // VALIDACAO AQUI || precisa? VERIFICAR CASO RETORNE ERRO AO NAO ACHAR O ID
+    const [rows] = await pool.query("DELETE FROM pessoa WHERE id = ?;", [id])
+    return rows
+}
+
+//CRUD FUNCIONARIO
+export async function insert_funcionario(id_pessoa, cargo, salario, data_admissao) {
+    //VALIDACAO AQUI \\ como vai funcionar na lógica o insert de funcionario e hospedes??
+    const [rows] = await pool.query("INSERT INTO funcionario(id_pessoa, cargo, salario, data_admissao) VALUES (?, ?, ?, ?);", [id_pessoa, cargo, salario, data_admissao])    
+    return rows
+}
+
+export async function select_funcionario(id) {
+    const [rows] = await pool.query("SELECT * FROM funcionario WHERE id = ?;", [id])
+    return rows
+}
+
+export async function update_pessoa(coluna, informacao, id) {
+    //VALIDACAO AQUI
+    const [rows] = await pool.query("UPDATE funcionario SET ? = ? WHERE id - ?;", [coluna, informacao, id])
+    return rows
+}
+
+export async function delete_pessoa(id) {
+    const [rows] = await pool.query("DELETE FROM funcionario WHERE id = ?;")
+    return rows
+}
+
+//CRUD HOSPEDE
+export async function insert_hospede(id_pessoa, data_criacao_conta, ultimo_login) {
+    //VALIDACAO AQUI || default pro ultimo login ao criar a conta é o data_criacao_conta
+    const [rows] = await pool.query("INSERT INTO hospede(id_pessoa, data_criacao_conta, ultimo_login) VALUES (")
+}
+
