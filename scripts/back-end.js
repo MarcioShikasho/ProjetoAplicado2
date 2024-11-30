4d3f3d64cc2b716a925d88e71977e22d74045cf6
A1f13n <alflen.fillipi08@gmail.com>
2024-11-29 23:01:23 -0300

    Atualização para entrega de hoje

--- scripts/back-end.js
+++ scripts/back-end.js
@@ -10,22 +10,54 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-export async function insert_hospede_e_pessoa(pessoaData, hospedeData) {
+export async function validarLogin(login, senha) {
+    const connection = await pool.getConnection();
+    
+    try {
+        await connection.beginTransaction();
+        const [loginResult] = await connection.query(
+            'SELECT * FROM pessoa WHERE login = ? AND senha = ?;',
+            [login, senha]
+        );
+    if (loginResult.length > 0) {
+        await connection.query(
+            'UPDATE hospede SET ultimo_login = CURDATE() WHERE id_pessoa = ?;',
+            [loginResult[0]]
+        );
+        console.log("Ultimo login atualizado!")
+        localStorage.setItem('userId', loginResult[0]);
+
+        await connection.commit();
+
+        return loginResult[0];
+    } else {
+        console.log("Usuário ou senha inválidos");
+        await connection.rollback();
+        return null;
+    }} catch (error) {
+        console.error("Erro durante o login: ", error)
+
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
+
+export async function insertHospedePessoa(pessoaData, hospedeData) {
     const connection = await pool.getConnection();
     try{
         await connection.beginTransaction();
 
-        // Inserir pessoa
         const [pessoaResult] = await connection.query(
-            'INSERT INTO pessoa (nome, cpf, login, senha, data_nasc, telefone, email) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [pessoaData.nome, pessoaData.cpf, pessoaData.login, pessoaData.senha, pessoaData.data_nasc, pessoaData.telefone, pessoaData.email]
+            'INSERT INTO pessoa (nome, cpf, senha, data_nasc, telefone, email) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [pessoaData.nome, pessoaData.cpf, pessoaData.senha, pessoaData.data_nasc, pessoaData.telefone, pessoaData.email]
         );
 
-        // Inserir hóspede
-        const pesosaId = pessoaResult.insertId;
+        const pessoaId = pessoaResult.insertId;
         await connection.query(
-            'INSERT INTO hospede(id_pessoa, data_criacao_conta, ultimo_login) VALUES (?, ?, ?);',
-            [pessoaResult.insertId, hospedeData.data_criacao_conta, hospedeData.ultimo_login]
+            'INSERT INTO hospede(id_pessoa, data_criacao_conta, ultimo_login) VALUES (?, CURDATE(), CURDATE());',
+            [pessoaResult.insertId]
             
         );
     
@@ -39,36 +71,94 @@ export async function insert_hospede_e_pessoa(pessoaData, hospedeData) {
         connection.release();
     }
 }
+ 
+
+// export async function select_hospede(id) {
+//     const connection = await pool.getConnection();
+//     try {
+//         await connection.beginTransaction();
+//         const [selectResult] = await connection.query(
+//             'SELECT * FROM pessoa WHERE id = ?;', id
+//         );
+//         if (selectResult.length === 0) {
+//             return null;
+//         }
+
+//         return selectResult;
+//     } catch (error) {
+//         console.error("Erro ao buscar dados", error);
+//         throw err
+//     }
+// }
 
 
-export async function insert_funcionario_e_pessoa(pessoaData, funcionarioData) {
+// export async function insertFuncionarioPessoa(pessoaData, funcionarioData) {
+
+//     const connection = await pool.getConnection();
+//     try{
+//         await connection.beginTransaction();
+
+//         const [pessoaResult] = await connection.query(
+//             'INSERT INTO pessoa (nome, cpf, senha, data_nasc, telefone, email) VALUES (?, ?, ?, ?, ?, ?, ?)',
+//             [pessoaData.nome, pessoaData.cpf, pessoaData.senha, pessoaData.data_nasc, pessoaData.telefone, pessoaData.email]
+//         );
+
+//         const pessoaId = pessoaResult.insertId;
+//         await connection.query(
+//             'INSERT INTO funcionario (id_pessoa, cargo, salario, data_admissao) VALUES (?, ?, ?, ?)',
+//             [pessoaId, funcionarioData.cargo, funcionarioData.salario, funcionarioData.data_admissao]
+//         );
+
+//         await connection.commit();
+//         return pessoaId; 
+//     } catch (error) {
+//         await connection.rollback(); // Reverte as operações em caso de erro
+//         console.error("Erro ao inserir pessoa e funcionário:", error);
+//         throw error;
+//     } finally {
+//         connection.release(); // Libera a conexão
+//     }
+// }
+
+
+export async function insertAcomodacao(acomodacaoData) {
     const connection = await pool.getConnection();
     try {
-        await connection.beginTransaction(); // Inicia a transação
+        await connection.beginTransaction();
 
-        // Inserir pessoa
-        const [pessoaResult] = await connection.query(
-            'INSERT INTO pessoa (nome, cpf, login, senha, data_nasc, telefone, email) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [pessoaData.nome, pessoaData.cpf, pessoaData.login, pessoaData.senha, pessoaData.data_nasc, pessoaData.telefone, pessoaData.email]
+        const [acomodacaoResult] = await connection.query(
+            'INSERT INTO acomodacao(nome, capacidade, numero_camas, descricao_acomodidades, valor_diario, disponibilidade',
+            [nome, capacidade, numero_camas, descricao_acomodidades, valor_diaria, disponibilidade]
         );
 
-        // Inserir funcionário
-        const pessoaId = pessoaResult.insertId;
-        await connection.query(
-            'INSERT INTO funcionario (id_pessoa, cargo, salario, data_admissao) VALUES (?, ?, ?, ?)',
-            [pessoaId, funcionarioData.cargo, funcionarioData.salario, funcionarioData.data_admissao]
-        );
-
-        await connection.commit(); // Confirma a transação
-        return pessoaId; // Retorna o ID da pessoa inserida
+        await connection.commit();
+        return acomodacaoResult.insertId;
     } catch (error) {
-        await connection.rollback(); // Reverte as operações em caso de erro
-        console.error("Erro ao inserir pessoa e funcionário:", error);
+        await connection.rollback();
+        console.error("Erro ao inserir acomodação", error);
         throw error;
     } finally {
-        connection.release(); // Libera a conexão
+        connection.release();
     }
 }
 
+export async function insertReserva(reservaData) {
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
 
-export async function insert_acomodacao()
+        const [reservaResult] = await connection.query(
+            'INSERT INTO reserva(id_hospede, id_acomodacao, id_funcionario, data_inicio, data_fim, valor, metodo_pgto, status_reserva, preferencias) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);',
+            [nome, capacidade, numero_camas, descricao_acomodidades, valor_diaria, disponibilidade]
+        );
+
+        await connection.commit();
+        return reservaResult.insertId;
+    } catch (error) {
+        await connection.rollback();
+        console.error("Erro ao inserir reserva", error);
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
